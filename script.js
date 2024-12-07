2bd078264acce7aeff0586d4c8be13f88eae9c3d
Boisxon <boisxon357@gmail.com>
2024-12-07 19:51:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+ var ism = prompt("Ismingizni kiriting ")
+  var familiya = prompt("Familiya kiriting ")
+  var yosh = +prompt("Yosh kiriting ")
+   console.log(`name ${ism}` )
+   console.log( `family ${familiya} `)
+   console.log(`age ${yosh}` )
+        
